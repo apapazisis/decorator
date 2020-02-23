3cb2aa29080f9a9cee2fa50eab1f5381db9b57fe
Apostolos Papazisis <info@apapazisis.de>
2020-02-23 15:10:14 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+interface UserRepositoryInterface
+{
+    public function all();
+}
+
+class UserRepository implements UserRepositoryInterface
+{
+    protected $model = 'User';
+    
+    public function all()
+    {
+        return $this->model;
+    }
+}
+
+abstract class UserDecorator implements UserRepositoryInterface
+{
+    protected $repository;
+     
+    public function __construct(UserRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+}
+
+class UserRepositoryDecorator extends UserDecorator
+{
+     public function all()
+     {
+         return Cache::remember('all', function()) {
+            return $this->repository->all();
+         });
+     }
+}
+
+
+$userRepository = new UserRepository();
+$userRepositoryDecorator = new UserRepositoryDecorator($userRepository);
+echo $userRepositoryDecorator->all();
+
